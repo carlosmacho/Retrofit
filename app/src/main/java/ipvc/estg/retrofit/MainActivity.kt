dbdb53a5bdae62faa182ee790ac62e817f165ac3
Carlos Macho <carlosmacho@ipvc.pt>
2023-05-06 14:01:07 +0100

    Fixed network permission error.

--- app/src/main/java/ipvc/estg/retrofit/MainActivity.kt
+++ app/src/main/java/ipvc/estg/retrofit/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ipvc.estg.retrofit.adapter.UserAdapter
 import ipvc.estg.retrofit.api.EndPoints
+import ipvc.estg.retrofit.api.OutputPost
 import ipvc.estg.retrofit.api.ServiceBuilder
 import ipvc.estg.retrofit.api.User
 import retrofit2.Call
@@ -20,22 +21,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         recyclerView = findViewById(R.id.recyclerView)
 
         val request = ServiceBuilder.buildService(EndPoints::class.java)
         val call = request.getUsers()
-        call.enqueue(object : Callback<List<User>> { override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) { if (response.isSuccessful){
-            recyclerView.apply {
-                setHasFixedSize(true)
-                layoutManager = LinearLayoutManager(this@MainActivity)
-                adapter = UserAdapter(response.body()!!) } } }
+
+        call.enqueue(object : Callback<List<User>> {
+            override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+                if (response.isSuccessful){
+                    recyclerView.apply {
+                        setHasFixedSize(true)
+                        layoutManager = LinearLayoutManager(this@MainActivity)
+                        adapter = UserAdapter(response.body()!!)
+                    }
+                }
+            }
             override fun onFailure(call: Call<List<User>>, t: Throwable) {
-                Toast.makeText(this@MainActivity, "${t.message}", Toast.LENGTH_SHORT).show() } })
+                Toast.makeText(this@MainActivity, "${t.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     fun getSingle(view: View){
         val request = ServiceBuilder.buildService(EndPoints::class.java)
         val call = request.getUserById(2)
+
         call.enqueue(object : Callback<User>{
             override fun onResponse(call: Call<User>, response: Response<User>) {
                 if (response.isSuccessful){
@@ -44,6 +55,23 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             override fun onFailure(call: Call<User>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "${t.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    fun post(view: View) {
+        val request = ServiceBuilder.buildService(EndPoints::class.java)
+        val call = request.postTest("teste")
+
+        call.enqueue(object : Callback<OutputPost>{
+            override fun onResponse(call: Call<OutputPost>, response: Response<OutputPost>) {
+                if (response.isSuccessful){
+                    val c: OutputPost = response.body()!!
+                    Toast.makeText(this@MainActivity, c.id.toString() + "-" + c.title, Toast.LENGTH_SHORT).show()
+                }
+            }
+            override fun onFailure(call: Call<OutputPost>, t: Throwable) {
                 Toast.makeText(this@MainActivity, "${t.message}", Toast.LENGTH_SHORT).show()
             }
         })
